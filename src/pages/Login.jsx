a7c59f2d4cdb47a9d1477a8e85987c6d168e3966
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2021-12-26 22:56:41 +0530

    adding login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,69 +1,92 @@
-import { useState, React } from 'react'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
-import './login.css'
-import Logo_D from '../assets/logo_d.png' 
-import contact from './login_assets/contact.png'
+import { useState, React } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./login.css";
+import Logo_D from "../assets/logo_d.png";
+import contact from "./login_assets/contact.png";
+import { send_otp } from "../api/index";
 
 const Login = () => {
+  const [enterNumber, setEnterNumber] = useState(true);
+  const [number, setNumber] = useState(null);
+  const [otp, setOtp] = useState(false);
 
-    const [enterNumber, setEnterNumber] = useState(true)
-    const [number, setNumber] = useState(null);
-    const [otp, setOtp] = useState(false);
+  const history = useHistory();
 
-    const history = useHistory();
+  function getNumber(val) {
+    setNumber(val.target.value);
+  }
 
-    function getNumber(val){
-        setNumber(val.target.value);
+  async function sendOtp() {
+    // Send OTP
+    console.log(number);
+    var body = {
+      mobile: number,
+    };
+    var res = await send_otp(body);
+    if (res.status == 200) {
+      setOtp(true);
+      setEnterNumber(false);
+    } else {
+      alert("Some Error Occurred");
     }
+  }
 
-    function displayNumber(){
-        console.log(number);
-        setOtp(true);
-        setEnterNumber(false);
-    }
+  function verifyOtp() {
+    // Verify OTP
+    console.log(otp);
+    history.push("/dashboard");
+  }
+  return (
+    <div className="login-body">
+      {enterNumber ? (
+        <form onSubmit={sendOtp}>
+          <div className="login">
+            <img className="login-img" src={Logo_D} alt="sayf_logo" />
+            <h3>Login </h3>
+            <div className="number-container">
+              <div className="login-text">
+                <img className="contact-logo" src={contact} alt="contact" />
+                Enter Contact Number
+              </div>
+              <input
+                required
+                placeholder="98754...."
+                type="text"
+                onChange={getNumber}
+              />{" "}
+              <br />
+              <button type="submit" className="otp-btn">
+                Get OTP
+              </button>{" "}
+              <br />
+            </div>
+          </div>
+        </form>
+      ) : (
+        <></>
+      )}
+      {otp ? (
+        <form onSubmit={verifyOtp}>
+          <div className="login">
+            <img className="login-img" src={Logo_D} alt="sayf_logo" />
+            <h3>Login </h3>
+            <div className="otp-container">
+              <p>OTP sent to {number}</p>
+              <input
+                required
+                placeholder="Enter OTP here..."
+                type="text"
+              />{" "}
+              <br />
+              <button className="otp-btn">Verify</button> <br />
+            </div>
+          </div>
+        </form>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
-    function verifyOtp(){
-        console.log(otp);
-        history.push('/dashboard')
-    }
-    return (
-        <div className='login-body'>
-                        {
-                            enterNumber? 
-                            <form onSubmit={displayNumber}>
-                                <div className="login">
-                                        <img className='login-img' src={Logo_D} alt="sayf_logo" />
-                                        <h3>Login </h3>
-                                <div className="number-container">
-                                    <div className="login-text">
-                                        <img className='contact-logo' src={contact} alt="contact" />Enter Contact Number
-                                    </div>
-                                    <input required placeholder='98754....' type="text" onChange={getNumber}/> <br />
-                                    <button type='submit' className='otp-btn'>Get OTP</button> <br />
-                                </div>
-                                </div>
-                            </form>
-                            :
-                            <></>
-                        }
-                        {
-                            otp? 
-                            <form onSubmit={verifyOtp}>
-                                <div className="login">
-                                        <img className='login-img' src={Logo_D} alt="sayf_logo" />
-                                        <h3>Login </h3>
-                                <div className="otp-container">
-                                    <p>OTP sent to {number}</p>
-                                    <input required placeholder='Enter OTP here...' type="text"/> <br />
-                                    <button className='otp-btn'>Verify</button> <br />
-                                </div>
-                                </div>
-                            </form>
-                            :
-                            <></>
-                        }
-                    </div>
-    )
-}
-
-export default Login
+export default Login;
